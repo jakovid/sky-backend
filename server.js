d49ad1cb1269963137437504915373fa49cf68fd
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-07-10 21:46:51 +0800

    began to add cloudinary routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const teacherRoutes = require('./routes/teachers')
+const imageRoutes = require('./routes/images')
 
 // express app
 const app = express()
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/teachers', teacherRoutes)
+app.use('/api/images', imageRoutes)
 
 //connect to DB
 mongoose.connect(process.env.MONGO_URI)
