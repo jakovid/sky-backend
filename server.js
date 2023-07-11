7af5bb7dc64d666db3dbb791e6e9ccac21485075
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-07-11 21:42:56 +0800

    cloudinary is difficult

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const teacherRoutes = require('./routes/teachers')
-const imageRoutes = require('./routes/images')
+
 
 // express app
 const app = express()
@@ -18,7 +18,6 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/teachers', teacherRoutes)
-app.use('/api/images', imageRoutes)
 
 //connect to DB
 mongoose.connect(process.env.MONGO_URI)
