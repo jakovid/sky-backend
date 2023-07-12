3f50bad9a54ec314894f9a883c2d5284f502d134
Jacob Dowdle <jacobk.dowdle@gmail.com>
2023-07-12 15:40:03 +0800

    moved cloudinary actions from frontend to back end and now can delete images from cloudinary

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const teacherRoutes = require('./routes/teachers')
+const imageRoutes = require('./routes/images')
 
 
 // express app
@@ -18,6 +19,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/teachers', teacherRoutes)
+app.use('/api/images', imageRoutes)
 
 //connect to DB
 mongoose.connect(process.env.MONGO_URI)
